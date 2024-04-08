aa1f50ce2e8444b6b2a830f0fd2c3cc1d155a758
minasjam <martin.minasjan@seznam.cz>
2024-04-08 14:06:52 +0200

    chore: add cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "cypress";
+import { env } from "~/env";
 
 export default defineConfig({
+  video: false,
+  defaultCommandTimeout: 8000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
