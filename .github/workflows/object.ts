2b85780e422cae04fb5a269720394a73f56f5e82
minasjam <martin.minasjan@seznam.cz>
2024-04-12 09:53:01 +0200

    ci: add cypress base url for short branch names

---
+++ .github/workflows/object.ts
@@ -0,0 +1,263 @@
+const object = {
+  token: "***",
+  job: "Explore-GitHub-Actions",
+  ref: "refs/heads/add-ci",
+  sha: "9fa36a859de11e6786019433ab5499f9bfa19df3",
+  repository: "minasjam/linear-copy",
+  repository_owner: "minasjam",
+  repository_owner_id: "91913291",
+  repositoryUrl: "git://github.com/minasjam/linear-copy.git",
+  run_id: "8646100928",
+  run_number: "36",
+  retention_days: "90",
+  run_attempt: "1",
+  artifact_cache_size_limit: "10",
+  repository_visibility: "public",
+  "repo-self-hosted-runners-disabled": false,
+  "enterprise-managed-business-id": "",
+  repository_id: "781347138",
+  actor_id: "91913291",
+  actor: "minasjam",
+  triggering_actor: "minasjam",
+  workflow: "GitHub Actions Demo",
+  head_ref: "",
+  base_ref: "",
+  event_name: "push",
+  event: {
+    after: "9fa36a859de11e6786019433ab5499f9bfa19df3",
+    base_ref: null,
+    before: "3fd0978410e3a28a67e32e4c2005090604f364f1",
+    commits: [
+      {
+        author: {
+          email: "martin.minasjan@seznam.cz",
+          name: "minasjam",
+          username: "minasjam",
+        },
+        committer: {
+          email: "martin.minasjan@seznam.cz",
+          name: "minasjam",
+          username: "minasjam",
+        },
+        distinct: true,
+        id: "9fa36a859de11e6786019433ab5499f9bfa19df3",
+        message: "ci: only TEST the app on the VERCEL build",
+        timestamp: "2024-04-11T13:41:59+02:00",
+        tree_id: "b6684f84d5823d8393ea81893bcc2d186d31ecd8",
+        url: "https://github.com/minasjam/linear-copy/commit/9fa36a859de11e6786019433ab5499f9bfa19df3",
+      },
+    ],
+    compare:
+      "https://github.com/minasjam/linear-copy/compare/3fd0978410e3...9fa36a859de1",
+    created: false,
+    deleted: false,
+    forced: false,
+    head_commit: {
+      author: {
+        email: "martin.minasjan@seznam.cz",
+        name: "minasjam",
+        username: "minasjam",
+      },
+      committer: {
+        email: "martin.minasjan@seznam.cz",
+        name: "minasjam",
+        username: "minasjam",
+      },
+      distinct: true,
+      id: "9fa36a859de11e6786019433ab5499f9bfa19df3",
+      message: "ci: only TEST the app on the VERCEL build",
+      timestamp: "2024-04-11T13:41:59+02:00",
+      tree_id: "b6684f84d5823d8393ea81893bcc2d186d31ecd8",
+      url: "https://github.com/minasjam/linear-copy/commit/9fa36a859de11e6786019433ab5499f9bfa19df3",
+    },
+    pusher: {
+      email: "91913291+minasjam@users.noreply.github.com",
+      name: "minasjam",
+    },
+    ref: "refs/heads/add-ci",
+    repository: {
+      allow_forking: true,
+      archive_url:
+        "https://api.github.com/repos/minasjam/linear-copy/{archive_format}{/ref}",
+      archived: false,
+      assignees_url:
+        "https://api.github.com/repos/minasjam/linear-copy/assignees{/user}",
+      blobs_url:
+        "https://api.github.com/repos/minasjam/linear-copy/git/blobs{/sha}",
+      branches_url:
+        "https://api.github.com/repos/minasjam/linear-copy/branches{/branch}",
+      clone_url: "https://github.com/minasjam/linear-copy.git",
+      collaborators_url:
+        "https://api.github.com/repos/minasjam/linear-copy/collaborators{/collaborator}",
+      comments_url:
+        "https://api.github.com/repos/minasjam/linear-copy/comments{/number}",
+      commits_url:
+        "https://api.github.com/repos/minasjam/linear-copy/commits{/sha}",
+      compare_url:
+        "https://api.github.com/repos/minasjam/linear-copy/compare/{base}...{head}",
+      contents_url:
+        "https://api.github.com/repos/minasjam/linear-copy/contents/{+path}",
+      contributors_url:
+        "https://api.github.com/repos/minasjam/linear-copy/contributors",
+      created_at: 1712131133,
+      default_branch: "main",
+      deployments_url:
+        "https://api.github.com/repos/minasjam/linear-copy/deployments",
+      description: null,
+      disabled: false,
+      downloads_url:
+        "https://api.github.com/repos/minasjam/linear-copy/downloads",
+      events_url: "https://api.github.com/repos/minasjam/linear-copy/events",
+      fork: false,
+      forks: 0,
+      forks_count: 0,
+      forks_url: "https://api.github.com/repos/minasjam/linear-copy/forks",
+      full_name: "minasjam/linear-copy",
+      git_commits_url:
+        "https://api.github.com/repos/minasjam/linear-copy/git/commits{/sha}",
+      git_refs_url:
+        "https://api.github.com/repos/minasjam/linear-copy/git/refs{/sha}",
+      git_tags_url:
+        "https://api.github.com/repos/minasjam/linear-copy/git/tags{/sha}",
+      git_url: "git://github.com/minasjam/linear-copy.git",
+      has_discussions: false,
+      has_downloads: true,
+      has_issues: true,
+      has_pages: false,
+      has_projects: true,
+      has_wiki: false,
+      homepage: "https://linear-copy.vercel.app",
+      hooks_url: "https://api.github.com/repos/minasjam/linear-copy/hooks",
+      html_url: "https://github.com/minasjam/linear-copy",
+      id: 781347138,
+      is_template: false,
+      issue_comment_url:
+        "https://api.github.com/repos/minasjam/linear-copy/issues/comments{/number}",
+      issue_events_url:
+        "https://api.github.com/repos/minasjam/linear-copy/issues/events{/number}",
+      issues_url:
+        "https://api.github.com/repos/minasjam/linear-copy/issues{/number}",
+      keys_url:
+        "https://api.github.com/repos/minasjam/linear-copy/keys{/key_id}",
+      labels_url:
+        "https://api.github.com/repos/minasjam/linear-copy/labels{/name}",
+      language: "TypeScript",
+      languages_url:
+        "https://api.github.com/repos/minasjam/linear-copy/languages",
+      license: null,
+      master_branch: "main",
+      merges_url: "https://api.github.com/repos/minasjam/linear-copy/merges",
+      milestones_url:
+        "https://api.github.com/repos/minasjam/linear-copy/milestones{/number}",
+      mirror_url: null,
+      name: "linear-copy",
+      node_id: "R_kgDOLpJpQg",
+      notifications_url:
+        "https://api.github.com/repos/minasjam/linear-copy/notifications{?since,all,participating}",
+      open_issues: 1,
+      open_issues_count: 1,
+      owner: {
+        avatar_url: "https://avatars.githubusercontent.com/u/91913291?v=4",
+        email: "91913291+minasjam@users.noreply.github.com",
+        events_url: "https://api.github.com/users/minasjam/events{/privacy}",
+        followers_url: "https://api.github.com/users/minasjam/followers",
+        following_url:
+          "https://api.github.com/users/minasjam/following{/other_user}",
+        gists_url: "https://api.github.com/users/minasjam/gists{/gist_id}",
+        gravatar_id: "",
+        html_url: "https://github.com/minasjam",
+        id: 91913291,
+        login: "minasjam",
+        name: "minasjam",
+        node_id: "U_kgDOBXp8Sw",
+        organizations_url: "https://api.github.com/users/minasjam/orgs",
+        received_events_url:
+          "https://api.github.com/users/minasjam/received_events",
+        repos_url: "https://api.github.com/users/minasjam/repos",
+        site_admin: false,
+        starred_url:
+          "https://api.github.com/users/minasjam/starred{/owner}{/repo}",
+        subscriptions_url:
+          "https://api.github.com/users/minasjam/subscriptions",
+        type: "User",
+        url: "https://api.github.com/users/minasjam",
+      },
+      private: false,
+      pulls_url:
+        "https://api.github.com/repos/minasjam/linear-copy/pulls{/number}",
+      pushed_at: 1712835723,
+      releases_url:
+        "https://api.github.com/repos/minasjam/linear-copy/releases{/id}",
+      size: 118,
+      ssh_url: "git@github.com:minasjam/linear-copy.git",
+      stargazers: 0,
+      stargazers_count: 0,
+      stargazers_url:
+        "https://api.github.com/repos/minasjam/linear-copy/stargazers",
+      statuses_url:
+        "https://api.github.com/repos/minasjam/linear-copy/statuses/{sha}",
+      subscribers_url:
+        "https://api.github.com/repos/minasjam/linear-copy/subscribers",
+      subscription_url:
+        "https://api.github.com/repos/minasjam/linear-copy/subscription",
+      svn_url: "https://github.com/minasjam/linear-copy",
+      tags_url: "https://api.github.com/repos/minasjam/linear-copy/tags",
+      teams_url: "https://api.github.com/repos/minasjam/linear-copy/teams",
+      topics: [],
+      trees_url:
+        "https://api.github.com/repos/minasjam/linear-copy/git/trees{/sha}",
+      updated_at: "2024-04-04T13:12:28Z",
+      url: "https://github.com/minasjam/linear-copy",
+      visibility: "public",
+      watchers: 0,
+      watchers_count: 0,
+      web_commit_signoff_required: false,
+    },
+    sender: {
+      avatar_url: "https://avatars.githubusercontent.com/u/91913291?v=4",
+      events_url: "https://api.github.com/users/minasjam/events{/privacy}",
+      followers_url: "https://api.github.com/users/minasjam/followers",
+      following_url:
+        "https://api.github.com/users/minasjam/following{/other_user}",
+      gists_url: "https://api.github.com/users/minasjam/gists{/gist_id}",
+      gravatar_id: "",
+      html_url: "https://github.com/minasjam",
+      id: 91913291,
+      login: "minasjam",
+      node_id: "U_kgDOBXp8Sw",
+      organizations_url: "https://api.github.com/users/minasjam/orgs",
+      received_events_url:
+        "https://api.github.com/users/minasjam/received_events",
+      repos_url: "https://api.github.com/users/minasjam/repos",
+      site_admin: false,
+      starred_url:
+        "https://api.github.com/users/minasjam/starred{/owner}{/repo}",
+      subscriptions_url: "https://api.github.com/users/minasjam/subscriptions",
+      type: "User",
+      url: "https://api.github.com/users/minasjam",
+    },
+  },
+  server_url: "https://github.com",
+  api_url: "https://api.github.com",
+  graphql_url: "https://api.github.com/graphql",
+  ref_name: "add-ci",
+  ref_protected: false,
+  ref_type: "branch",
+  secret_source: "Actions",
+  workflow_ref:
+    "minasjam/linear-copy/.github/workflows/github-demo.yml@refs/heads/add-ci",
+  workflow_sha: "9fa36a859de11e6786019433ab5499f9bfa19df3",
+  workspace: "/home/runner/work/linear-copy/linear-copy",
+  action: "__run_9",
+  event_path: "/home/runner/work/_temp/_github_workflow/event.json",
+  action_repository: "",
+  action_ref: "",
+  path: "/home/runner/work/_temp/_runner_file_commands/add_path_abe29c8f-0e44-44b0-8a3c-b4f458e036cb",
+  env: "/home/runner/work/_temp/_runner_file_commands/set_env_abe29c8f-0e44-44b0-8a3c-b4f458e036cb",
+  step_summary:
+    "/home/runner/work/_temp/_runner_file_commands/step_summary_abe29c8f-0e44-44b0-8a3c-b4f458e036cb",
+  state:
+    "/home/runner/work/_temp/_runner_file_commands/save_state_abe29c8f-0e44-44b0-8a3c-b4f458e036cb",
+  output:
+    "/home/runner/work/_temp/_runner_file_commands/set_output_abe29c8f-0e44-44b0-8a3c-b4f458e036cb",
+};
