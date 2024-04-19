6ee1a53668a3abc2da1f0ade0e98f9d492da8ae7
minasjam <martin.minasjan@seznam.cz>
2024-04-19 12:51:34 +0200

    ci: clean up test file

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,17 @@
 import { LandingAssertions } from "cypress/support/assertions/LandingAssertions";
-import { LandingSteps } from "cypress/support/steps/LandingSteps";
 import { MainAssertions } from "cypress/support/assertions/MainAsserions";
 import { MainSteps } from "cypress/support/steps/MainSteps";
 
-describe.only("template spec", () => {
-  it("passes", () => {
+describe.only("App run through", () => {
+  beforeEach(function () {
     cy.visit("/");
-
     LandingAssertions.titleIsVisible();
     LandingAssertions.logInButtonIsVisible();
-    // LandingSteps.selectLogInButton();
-
-    // cy.login();
     cy.loginToAuth0("martin.minasjan+test1@profiq.com", "jD7qfeCqgJQwE5s");
     cy.visit("/");
+  });
 
+  it("Logs in through branches", () => {
     MainAssertions.titleIsVisible();
     MainAssertions.logOutButtonIsVisible();
     MainAssertions.subtitleIsVisible();
@@ -24,21 +21,5 @@ describe.only("template spec", () => {
 
     MainSteps.selectDeleteTaskButton();
     MainSteps.selectLogOutButton();
-  });
-});
-
-describe("Auth0", function () {
-  beforeEach(function () {
-    // cy.task("db:seed");
-    cy.visit("/");
-    cy.loginToAuth0("martin.minasjan+test1@profiq.com", "jD7qfeCqgJQwE5s");
-    cy.visit("/");
-  });
-
-  it("shows onboarding", function () {
-    MainAssertions.titleIsVisible();
-    MainAssertions.logOutButtonIsVisible();
-    MainAssertions.subtitleIsVisible();
-    MainAssertions.taskInputIsVisible();
   });
 });
