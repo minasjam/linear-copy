2b2bac416fea86373e592f14857655e544b4ef0e
minasjam <martin.minasjan@seznam.cz>
2024-04-25 08:37:33 +0200

    test: tweak logging in through auth0

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
+  defaultCommandTimeout: 8000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+  // env: {
+  //   auth0_username: process.env.AUTH0_USERNAME,
+  //   auth0_password: process.env.AUTH0_PASSWORD,
+  //   auth0_domain: process.env.REACT_APP_AUTH0_DOMAIN,
+  //   auth0_audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  //   auth0_scope: process.env.REACT_APP_AUTH0_SCOPE,
+  //   auth0_client_id: process.env.REACT_APP_AUTH0_CLIENTID,
+  //   auth0_client_secret: process.env.AUTH0_CLIENT_SECRET,
+  // },
 });
