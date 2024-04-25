157c6bf44152c1af9a28eac5221b7ccbe80236ec
minasjam <martin.minasjan@seznam.cz>
2024-04-25 08:38:47 +0200

    feat: enable multi tenancy in auth0

--- src/utils/auth0.js
+++ src/utils/auth0.js
@@ -1,21 +1,28 @@
-import { env } from "~/env";
+// import { env } from "~/env";
 import { initAuth0 } from "@auth0/nextjs-auth0";
 
-export function returnAuth0BaseURL() {
-  switch (env.VERCEL_ENV) {
-    case "production":
-      return env.AUTH0_BASE_URL;
-    case "preview":
-      return env.VERCEL_BRANCH_URL
-        ? `https://${env.VERCEL_BRANCH_URL}`
-        : "https://linear-copy.vercel.app";
-    default:
-      return env.AUTH0_BASE_URL || "http://localhost:3000";
-  }
-}
+// const returnURL = () => {
+//   // const currentUrl = window.location.href;
+//   // console.log(currentUrl);
+//   return env.AUTH0_BASE_URL || "http://localhost:3000";
+// };
 
-const baseURL = returnAuth0BaseURL();
+// export function returnAuth0BaseURL() {
+//   switch (env.VERCEL_ENV) {
+//     case "production":
+//       return env.AUTH0_BASE_URL;
+//     case "preview":
+//       return env.VERCEL_COMMIT_URL
+//         ? `https://${env.VERCEL_COMMIT_URL}`
+//         : "https://linear-copy.vercel.app";
+//     default:
+//       return returnURL();
+//   }
+// }
 
-export default initAuth0({
-  baseURL,
-});
+// const baseURL = returnAuth0BaseURL();
+
+export default initAuth0();
+//   {
+//   baseURL,
+// }
